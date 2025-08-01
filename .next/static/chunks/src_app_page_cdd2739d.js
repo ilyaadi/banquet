228f66c0ce54aab880b7da93acbab40db0b1aa7f
ilyaadi <aadi.sh2307@gmail.com>
2025-08-01 22:33:36 +0530

    save

---
+++ .next/static/chunks/src_app_page_cdd2739d.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_ca58869a._.js",
+  "static/chunks/src_3e4fedbe._.js",
+  "static/chunks/node_modules_aos_dist_aos_9414069c.css"
+],
+    source: "dynamic"
+});
